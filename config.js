d7340c6695ff46e9d09d0e5fdcbb9c9c20701be6
daria marczak <marczak.ria@gmail.com>
2018-01-06 18:24:59 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "GOOGLE_CLIENT_ID": "843214253203-6i7q129df0mbt7ipfk01s5panip9j6m4.apps.googleusercontent.com",
+  "GOOGLE_CLIENT_SECRET": "M270D68vHMhRbTQiP9rs_Wcu",
+  "CALLBACK_URL": "http://localhost:3000/auth/google/callback"
+}
